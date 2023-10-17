31e5c744532a99eb829c803d665b6b045a8d82e4
anarsantos <aninha_rsantos@yahoo.com.br>
2023-10-17 11:40:18 -0300

    States e Hooks

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Button, Alert } from 'react-native';
 import { css } from './assets/css/Css';
 import Page from './node_modules/views/Page';
 
 export default function App() {
+  const [product,setProduct]=useState('bola');
+  const [quantity,setQuantity]=useState(0);
+
+  useEffect(()=>{
+    if(quantity > 0) {
+      Alert.alert("Novo produto adicionado.");
+    }
+  });
+
   /*Spread*/
   const props={
     empresa: 'Webdesign em Foco',
-    name: 'Thiago'
+    name: 'Thiago',
+    produto: product,
+    quantidade: quantity
   };
 
   return (
     <View style={css.container}>
-      <Text style={css.textPage}>Open up App.js to start working on your app!</Text>      
+      <Text>{product}</Text>      
       <Page {...props} />
+      <Button title='Adicionar produtos' onPress={()=>setQuantity(quantity + 1)} />
       {/*<Page empresa='Webdesign' name='Thiago'/>*/}
       {/*<Page empresa='Programação'/>
       <Page empresa='Devs'/>
