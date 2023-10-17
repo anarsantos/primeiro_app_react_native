2be64fe961beac741a014271b9ed90bb82d126c9
anarsantos <aninha_rsantos@yahoo.com.br>
2023-10-17 10:42:18 -0300

    Primeiro commit react native

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { css } from './assets/css/Css';
+import Page from './node_modules/views/Page';
 
 export default function App() {
+  /*Spread*/
+  const props={
+    empresa: 'Webdesign em Foco',
+    name: 'Thiago'
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={css.container}>
+      <Text style={css.textPage}>Open up App.js to start working on your app!</Text>      
+      <Page {...props} />
+      {/*<Page empresa='Webdesign' name='Thiago'/>*/}
+      {/*<Page empresa='Programação'/>
+      <Page empresa='Devs'/>
+  <StatusBar style="auto" />*/}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
