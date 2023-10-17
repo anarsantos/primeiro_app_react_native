89a16f4732e042f49f6f8093521263f0ee4fcce6
anarsantos <aninha_rsantos@yahoo.com.br>
2023-10-17 12:52:41 -0300

    Navegação entre telas

--- App.js
+++ App.js
@@ -1,37 +1,21 @@
 import React, {useState, useEffect} from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { Text, View, Button, Alert } from 'react-native';
 import { css } from './assets/css/Css';
-import Page from './node_modules/views/Page';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './assets/views/Home';
+import Login from './assets/views/Login';
 
 export default function App() {
-  const [product,setProduct]=useState('bola');
-  const [quantity,setQuantity]=useState(0);
-
-  useEffect(()=>{
-    if(quantity > 0) {
-      Alert.alert("Novo produto adicionado.");
-    }
-  });
-
-  /*Spread*/
-  const props={
-    empresa: 'Webdesign em Foco',
-    name: 'Thiago',
-    produto: product,
-    quantidade: quantity
-  };
+  const Stack = createNativeStackNavigator();
 
   return (
-    <View style={css.container}>
-      <Text>{product}</Text>      
-      <Page {...props} />
-      <Button title='Adicionar produtos' onPress={()=>setQuantity(quantity + 1)} />
-      {/*<Page empresa='Webdesign' name='Thiago'/>*/}
-      {/*<Page empresa='Programação'/>
-      <Page empresa='Devs'/>
-  <StatusBar style="auto" />*/}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
